6800b27c491429fc5b54264782ae382f467f2176
Scott Pearson <162385516+ScottPearson14@users.noreply.github.com>
2025-06-08 21:35:39 -0500

    Add files via upload

---
+++ ArabicToRoman/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ArabicToRoman"},{"p":"<Unnamed>","l":"ArabicToRomanController"},{"p":"<Unnamed>","l":"ArabicToRomanConverterModel"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
